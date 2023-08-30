3bb0507522837402637e5583df4afb2715bf43ea
Allen Pan <allpan@email.com>
2023-08-29 20:11:42 -0700

    Update run file

--- const.py
+++ const.py
@@ -1,20 +1,22 @@
 VSA_MODEL = 'MAP'
 
-NUM_DIMENSIONS = 1000
-NUM_FACTORS = 2
-NUM_CODEVECTORS = 5
+# Default values
+DIM = 1000
+FACTORS = 3
+CODEVECTORS = 3
+# CODEVECTORS = [3, 3, 5, 7]
+NOISE_LEVEL = 0.0    # compositional vector noise
+ITERATIONS = 100  # max number of iterations for factorization
+NORMALIZE = False    # normalize the initial estimate and the input vector
+ACTIVATION = 'NONE' # 'NONE', 'ABS'
 
+assert(type(CODEVECTORS) == int or len(CODEVECTORS) == FACTORS)
 
-# Test range and step size
-MAX_DIMENSIONS = 10000
-STEP_DIMENSIONS = 1000
-MAX_FACTORS = 10
-STEP_FACTORS = 1
-MAX_CODEVECTORS = 50
-STEP_CODEVECTORS = 5
-
-# Compositional vector noise
-NOISE_LEVEL = 0.00
-
-NUM_ITERATIONS = 100
-ACTIVATION = 'NONE'
+# Test ranges
+DIM_RANGE = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+FACTOR_RANGE = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+CODEVECTOR_RANGE = [2, 3, 6, 10, 15, 20, 25, 30, 40, 50]
+NORMALIZE_RANGE = [False, True]
+ACTIVATION_RANGE = ['NONE', 'ABS']
+NOISE_RANGE = [0.0, 0.02, 0.05, 0.1, 0.15, 0.2, 0.3]
+ITERATION_RANGE = [100, 1000, 5000, 10000]
--- run.py
+++ run.py
@@ -9,14 +9,15 @@ from tqdm import tqdm
 import itertools
 import json
 from const import *
+import time
 
 # %%
-RUN_SINGLE = False
+RUN_MODE = "n-i" # "single", "d-f-v", "n-i"
+VERBOSE = 2
 NUM_SAMPLES = 400 # test data
 
-assert(type(NUM_CODEVECTORS) == int or len(NUM_CODEVECTORS) == NUM_FACTORS)
 
-def codevector_filename(num_codevectors):
+def v_name(num_codevectors):
     if type(num_codevectors) == int:
         return f"{num_codevectors}v"
     else:
@@ -25,7 +26,19 @@ def codevector_filename(num_codevectors):
             s = f"{num_codevectors[i]},"
         return s[:-1] + "v"
 
-def resonator(input,
+
+def normalize(input):
+    if isinstance(input, hd.MAPTensor):
+        return hd.hard_quantize(input)
+    elif isinstance(input, hd.BSCTensor):
+        # positive = torch.tensor(True, dtype=input.dtype, device=input.device)
+        # negative = torch.tensor(False, dtype=input.dtype, device=input.device)
+        # return torch.where(input >= 0, positive, negative)
+
+        # Seems like BSCTensor is automatically normalized after bundle
+        return input
+
+def resonator_stage(input,
               estimates: hd.VSATensor or list,
               codebooks: hd.VSATensor or list,
               activation: Literal['NONE', 'ABS', 'ZERO'] = 'NONE'
@@ -64,12 +77,12 @@ def resonator(input,
         return output
 
 
-
-def factorization(input, estimates, codebooks):
-
+def resonator_network(input, estimates, codebooks, iterations=ITERATIONS, normalize=NORMALIZE, activation=ACTIVATION):
     old_estimates = estimates.clone()
-    for k in range(NUM_ITERATIONS):
-        estimates = resonator(input, estimates, codebooks, activation=ACTIVATION)
+    if normalize:
+        input = normalize(input)
+    for k in range(iterations):
+        estimates = resonator_stage(input, estimates, codebooks, activation=activation)
         if all((estimates == old_estimates).flatten().tolist()):
             break
         old_estimates = estimates.clone()
@@ -77,10 +90,18 @@ def factorization(input, estimates, codebooks):
     return estimates, k 
 
 
-def run(m,d,f,v, verbose=False):
-    print(Fore.BLUE + f"Running test with {m} model, {d} dimensions, {f} factors, {v} codevectors" + Fore.RESET)
+def run_factorization(
+        m = VSA_MODEL,
+        d = DIM,
+        f = FACTORS,
+        v = CODEVECTORS,
+        n = NOISE_LEVEL,
+        it = ITERATIONS,
+        nom = NORMALIZE,
+        act = ACTIVATION,
+        verbose = 0):
 
-    test_dir = f"tests/{m}-{d}d-{f}f-{codevector_filename(v)}"
+    test_dir = f"tests/{m}-{d}d-{f}f-{v_name(v)}"
 
     vsa = VSA(
         root=test_dir,
@@ -91,68 +112,125 @@ def run(m,d,f,v, verbose=False):
     )
 
     # Generate test samples
-    sample_file = os.path.join(test_dir, f"samples-{NUM_SAMPLES}s-{NOISE_LEVEL}n.pt")
+    sample_file = os.path.join(test_dir, f"samples-{NUM_SAMPLES}s-{n}n.pt")
     if (os.path.exists(sample_file)):
         labels, samples = torch.load(sample_file)
     else:   
-        labels, samples = vsa.sample(NUM_SAMPLES, num_vectors_supoerposed=1, noise=NOISE_LEVEL)
+        labels, samples = vsa.sample(NUM_SAMPLES, num_vectors_supoerposed=1, noise=n)
         torch.save((labels, samples), sample_file)
 
     codebooks = torch.stack(vsa.codebooks)
     init_estimate = hd.multiset(codebooks)
+    if nom:
+        init_estimate = normalize(init_estimate)
 
     incorrect = 0
-    unconverged = 0
-    for i in tqdm(range(len(labels))):
-        input = samples[i]
-        label = labels[i]
+    unconverged = [0, 0] # Unconverged successful, unconverged failed
+    for j in tqdm(range(len(labels))):
+        input = samples[j]
+        label = labels[j]
 
-        output, convergence = factorization(input, init_estimate, codebooks)
+        output, convergence = resonator_network(input, init_estimate, codebooks, iterations=it, normalize=nom, activation=act)
 
-        # values: the similarity values
-        # indices: the indices of the codevectors in the codebooks
-        values, outcome = vsa.cleanup(output)
-
-        if (convergence == NUM_ITERATIONS-1):
-            unconverged += 1
+        # outcome: the indices of the codevectors in the codebooks
+        outcome = vsa.cleanup(output)
 
         if (outcome not in label):
             incorrect += 1
-            if verbose:
-                print(Fore.RED + f"Test {i} failed:" + f"Label = {label}, Outcome = {outcome}" + Fore.RESET)
+            unconverged[1] += 1 if convergence == it-1 else 0
+            if verbose >= 1:
+                print(Fore.RED + f"Test {j} failed:" + f"Label = {label}, Outcome = {outcome}" + Fore.RESET)
+                print(f"Convergence: {convergence}")
+        else:
+            unconverged[0] += 1 if convergence == it-1 else 0
+            if verbose >= 2:
+                print(f"Test {j} passed:" + f"Label = {label}, Outcome = {outcome}")
                 print(f"Convergence: {convergence}")
         
     accuracy = (NUM_SAMPLES - incorrect) / NUM_SAMPLES
     print(f"Accuracy: {accuracy}")
     print(f"Unconverged: {unconverged}/{len(labels)}")
-    return accuracy, unconverged
+
+    return accuracy, unconverged, {str((m, d, f, v, n, it, nom, act)): (accuracy, unconverged)}
+
+# Test various dimensions, factors, and codevectors
+def test_dim_fac_vec():
+    print(Fore.CYAN + f"Test Setup: model = {VSA_MODEL}, normalize = {NORMALIZE}, activation = {ACTIVATION}, noise = {NOISE_LEVEL}, iterations = {ITERATIONS}, samples = {NUM_SAMPLES}" + Fore.RESET)
+
+    table = {}
+    for d in DIM_RANGE:
+        skip_rest_f = False
+        for f in FACTOR_RANGE:
+            if not skip_rest_f:
+                skip_rest_v = False 
+            for v in CODEVECTOR_RANGE:
+                print(Fore.BLUE + f"Running test with {d} dimensions, {f} factors, {v} codevectors" + Fore.RESET)
+
+                if skip_rest_v:
+                    print(Fore.YELLOW + f"Skipping {d}d-{f}f-{v_name(v)}" + Fore.RESET)
+                    continue
+
+                accuracy, _, entry = run_factorization(d=d, f=f, v=v)
+
+                # If accuracy is less than 30%, skip the rest of the tests
+                if accuracy <= 0.3:
+                    skip_rest_v = True
+                    # If the first stage fails, skip the rest of f
+                    if v == CODEVECTOR_RANGE[0]:
+                        skip_rest_f = True
+                
+                table.update(entry)
+
+    json_file = f'tests/table-{VSA_MODEL}-{ITERATIONS}i-{NOISE_LEVEL}n-{"nom" if NORMALIZE else ""}-{ACTIVATION.lower() if ACTIVATION != "NONE" else ""}.json'
+    with open(json_file, 'w') as f:
+        json.dump(table, f)
+        print(Fore.GREEN + f"Saved table to {json_file}" + Fore.RESET)
+
+def test_noise_iter():
+    print(Fore.CYAN + f"Test Setup: model = {VSA_MODEL}, normalize = {NORMALIZE}, activation = {ACTIVATION}, dim = {DIM}, factors = {FACTORS}, codevectors = {CODEVECTORS}, samples = {NUM_SAMPLES}" + Fore.RESET)
+
+    table = {}
+    skip_rest_n = False
+    for n in NOISE_RANGE:
+        if not skip_rest_n:
+            skip_rest_i = False
+        for it in ITERATION_RANGE:
+            if skip_rest_i:
+                print(Fore.YELLOW + f"Skipping {n}n-{it}i" + Fore.RESET)
+                continue
+
+            print(Fore.BLUE + f"Running test with noise = {n}, iterations = {it}" + Fore.RESET)
+            accuracy, unconverged, entry = run_factorization(n=n, it=it)
+            # If always converged, more iterations don't matter
+            if (unconverged[0] + unconverged[1]) == 0:
+                skip_rest_i = True
+            # If accuracy is less than 10% for current noise level and more iterations don't have, skip
+            if accuracy <= 0.1 and skip_rest_i:
+                skip_rest_n = True
+
+            table.update(entry)
+
+    json_file = f'tests/table-{VSA_MODEL}-{DIM}d-{FACTORS}f-{CODEVECTORS}v{"-nom" if NORMALIZE else ""}{"-" + ACTIVATION.lower() if ACTIVATION != "NONE" else ""}.json'
+    with open(json_file, 'w') as f:
+        json.dump(table, f)
+        print(Fore.GREEN + f"Saved table to {json_file}" + Fore.RESET)
 
 
 if __name__ == '__main__':
     table = {}
 
-    if RUN_SINGLE:
-        run(VSA_MODEL, NUM_DIMENSIONS, NUM_FACTORS, NUM_CODEVECTORS, verbose=True)
-    else:
-        for d in range(NUM_DIMENSIONS, MAX_DIMENSIONS+1, STEP_DIMENSIONS):
-            skip_rest_f = False
-            for f in range(NUM_FACTORS, MAX_FACTORS+1, STEP_FACTORS):
-                if not skip_rest_f:
-                    skip_rest_v = False 
-                for v in range(NUM_CODEVECTORS, MAX_CODEVECTORS+1, STEP_CODEVECTORS):
-                    if skip_rest_v:
-                        print(Fore.YELLOW + f"Skipping {VSA_MODEL}-{d}d-{f}f-{codevector_filename(v)}" + Fore.RESET)
-                        continue
-                    accuracy, unconverged = run(VSA_MODEL, d,f,v, verbose=False)
-                    table[str((VSA_MODEL, d, f, v))] = (accuracy, unconverged)
-                    # If accuracy is less than 30%, skip the rest of the tests
-                    if (accuracy <= 0.3):
-                        skip_rest_v = True
-                        # If the first stage fails, skip the rest of f
-                        if (v == NUM_CODEVECTORS):
-                            skip_rest_f = True
-        
-        with open(f'tests/table-{NUM_ITERATIONS}i-{NOISE_LEVEL}n.json', 'w') as f:
-            json.dump(table, f)
+    start = time.time()
+    if RUN_MODE == "single":
+        run_factorization(verbose=VERBOSE)
+    elif RUN_MODE == "d-f-v":
+        test_dim_fac_vec()
+    elif RUN_MODE == "n-i":
+        test_noise_iter()
+    # elif RUN_MODE == ""
+
+    end = time.time()
+    print(f"Time elapsed: {end - start}s")
+
+
     
 # %%
